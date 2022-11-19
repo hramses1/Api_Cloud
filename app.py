60498f4db92154a132e4adcf2de6a8e691660992
hramses1 <79066311+hramses1@users.noreply.github.com>
2022-11-19 01:40:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes.routes import routes
 
 app=Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"]='mysql://root:4471@localhost:3306/estudiantes'
+app.config["SQLALCHEMY_DATABASE_URI"]='*********'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 SQLAlchemy(app)
 
